c6502fd31def68bf87158cccba95baa80f74b100
Ilya Brant <detroitib31@gmail.com>
2022-12-07 19:51:33 +0600

    Написа цикл с использованием всех ранее описанных функций

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,30 @@
+import java.util.*
+
 fun main(args: Array<String>) {
+
+    while (true) {
+        val n =  promtDouble("Введите десятичное число: ")
+        if (n == 0) {
+            break
+        }
+        displayln(binary(n))
+    }
+}
 
 fun display(text: String) {
     print(text)
 }
+
 fun displayln(text: String) {
     println(text)
 }
+
 fun promtDouble(text: String) : Int {
     val reader = Scanner(System.`in`)
     display(text)
     return reader.nextInt()
+}
+
 fun binary (n: Int): String {
     return if (n != 0) binary(n / 2) + n % 2 else { "" }
 }
